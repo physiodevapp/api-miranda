cf75fd8f3612aebc211ea5fec4dc4b41f5b5f0a1
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-07-24 20:51:05 +0200

    feat: add testing User routes

--- tests/routes.test.ts
+++ tests/routes.test.ts
@@ -2,8 +2,17 @@
 import request from "supertest";
 import { app } from "../app";
 import { generateToken } from "../utils/token";
+import userDataList from '../data/users.json';
 
-describe("Routes testing", () => {
+describe("Testing public routes", () => {
+  test("Root route returns status code 200", async () => {
+    const response = await request(app).get("/");
+ 
+    expect(response.status).toEqual(200);
+  });
+})
+
+describe("Testing User routes", () => {
   let payload: { email: string, password: string };
   let token: string;
 
@@ -15,15 +24,8 @@ describe("Routes testing", () => {
 
     token = generateToken(payload);
   });
-  
 
-  test("Public route returns status code 200", async () => {
-    const response = await request(app).get("/");
- 
-    expect(response.status).toEqual(200);
-  });
-
-  test("Users route returns status code 200 with valid token", async () => {
+  test("Users route returns status code 200 if valid token", async () => {
     const response = await request(app)
       .get("/users")
       .set("authorization", `Bearer ${token}`); 
@@ -31,12 +33,34 @@ describe("Routes testing", () => {
     expect(response.status).toEqual(200);
   });
 
-  test("Users route returns status code 401 with invalid token", async () => {
+  test("Users route returns status code 500 if invalid token", async () => {
     const response = await request(app)
       .get("/users")
       .set("authorization", `Bearer ${token}lj`); 
     
     expect(response.status).toEqual(500);
+  });
+
+  test("Users route returns status code 401 if invalid credentials", async () => {
+    payload = {
+      email: "admin.miranda@example.co",
+      password: "0000"
+    }
+    token = generateToken(payload);
+
+    const response = await request(app)
+      .get("/users")
+      .set("authorization", `Bearer ${token}`); 
+    
+    expect(response.status).toEqual(401);
+  });
+
+  test("Users route returns users if valid credentials", async () => {
+    const response = await request(app)
+      .get("/users")
+      .set("authorization", `Bearer ${token}`); 
+    
+    expect(response.body).toEqual(userDataList);
   });
 
 });
