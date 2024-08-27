7febe1efab22bcab5480d5d280de3c92c154f2de
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-08-27 18:35:00 +0200

    Revert "Merge pull request #30 from physiodevapp/mysql"
    This reverts commit 087b9499d3bd0a5e7759a90bd5510a8df7a25961, reversing
    changes made to 4fae186eb1562389d76a003f47f252a0c380fe22.

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ import { APIError } from './utils/APIError';
 import mustache from "mustache";
 import fs from 'fs';
 import { headers } from './middlewares/response.middleware';
+import { connectDB } from './config/db.config';
 import { handleError } from './controllers/error.controller';
 import serverless from 'serverless-http';
 import cors from 'cors';
@@ -22,6 +23,9 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
+
+if (process.env.NODE_ENV !== 'test') 
+  connectDB();
 
 app.use(logger("dev"));
 
--- src/config/dbMySQL.config.ts
+++ src/config/dbMySQL.config.ts
@@ -6,18 +6,12 @@ const loadEnvConfig = require('../config/loadEnv.config');
 if (process.env.NODE_ENV !== 'production')
   loadEnvConfig();
 
-export const getPool = async () => {
-  let pool;
-
-  if (!pool) {
-    pool = mysql.createPool({
+export const createPool = async () => {
+  return mysql.createPool({
       host: 'localhost',
       user: 'root',
       password: process.env.MYSQL_DB_PASSWORD,
       database: 'miranda',
       // connectionLimit: 10 // Optional: specify the maximum number of connections in the pool
-    });
-  }
-
-  return pool
+  });
 };
