6a2eaecff5db6c56505a996f0a8df881d0a56a84
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-08-27 17:52:52 +0200

    feat: contacts seed & mysql config

---
+++ src/config/dbMySQL.config.ts
@@ -0,0 +1,17 @@
+
+import mysql from 'mysql2/promise';
+
+const loadEnvConfig = require('../config/loadEnv.config');
+
+if (process.env.NODE_ENV !== 'production')
+  loadEnvConfig();
+
+export const createPool = async () => {
+  return mysql.createPool({
+      host: 'localhost',
+      user: 'root',
+      password: process.env.MYSQL_DB_PASSWORD,
+      database: 'miranda',
+      // connectionLimit: 10 // Optional: specify the maximum number of connections in the pool
+  });
+};
