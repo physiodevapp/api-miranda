5dd862c226f55a1ce2cc6d7445d116dc77b83442
Edu Gamboa <69968873+physiodevapp@users.noreply.github.com>
2024-10-04 10:06:35 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,5 @@ import { app } from "./app";
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`The app is running at port ${port}`);
+  console.log('action testing');
 })
